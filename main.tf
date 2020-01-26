dead7da9c0a62b6d2d013c054f36f7801020de56
NAzebiah <Noel.Azebiah@nfinancial.co.uk>
2020-01-26 05:05:52 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 provider "azurerm" {
   version = "=1.5.0"
-  subscription_id = "8f2249c6-4b25-49a0-81f3-0609add26601"
-    client_id       = "866aa0eb-da5c-42d4-84d0-fafaa2356698"
-    client_secret   = "3d563c74-09ae-43fc-bc64-b42119535ec0"
-    tenant_id       = "65b500d3-0c85-4060-88bb-c780ee64a1b1"
+  subscription_id = "xxxxx"
+    client_id       = "xxxxx"
+    client_secret   = "xxxxx"
+    tenant_id       = "xxxxx"
 }
 
 provider "kubernetes" {
